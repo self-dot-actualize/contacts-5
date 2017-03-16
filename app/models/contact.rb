b436fb527b4d862a5503b7dfd0693bef1a22636b
Peter Jang <peter.x.jang@gmail.com>
2017-03-15 20:10:31 -0500

    Add full name and japan phone number model methods

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ApplicationRecord
+  def full_name
+    "#{last_name}, #{first_name}"
+  end
+
+  def japan_phone_number
+    "+81 #{phone}"
+  end
+
+  def self.find_all_johns
+    Contact.where(first_name: "John")
+  end
 end
