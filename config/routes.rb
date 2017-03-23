9c2fbd02200ea5f6f52b6a2b0dd9bc7abc4484f5
Peter Jang <peter.x.jang@gmail.com>
2017-03-23 18:43:07 -0500

    Add authentication sign up feature

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get "/contacts/:id/edit" => "contacts#edit"
   patch "/contacts/:id" => "contacts#update"
   delete "/contacts/:id" => "contacts#destroy"
+
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
 end
